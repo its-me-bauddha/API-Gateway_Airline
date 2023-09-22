b35c73a0775eba6cdb39d68aad096713cebc202f
its-me-bauddha <harshbauddha563@gmail.com>
2023-09-22 11:01:48 +0530

    signin with jwt creation

--- src/repositories/user-repository.js
+++ src/repositories/user-repository.js
@@ -1,9 +1,15 @@
 const CrudRepository = require('./crud-repository')
-const {User} = require('../models')
+const {User} = require('../models');
+const { use } = require('bcrypt/promises');
 
 class UserRepository extends CrudRepository {
     constructor (){
-        super(User)
+        super(User);
+    }
+
+    async getUserByEmail(email){
+        const user  =await User.findOne({where:{email:email}});
+        return user;
     }
 }
 
